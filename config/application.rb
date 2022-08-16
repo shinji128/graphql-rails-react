bef8126c54e338553c2b3dc3fe15b7aad502005e
miura <s.miura@mof-mof.co.jp>
2022-08-16 23:48:10 +0900

    allでデータ取得

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module GraphqlRailsReact
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  end
+  post "/graphql", to: "graphql#execute"
   root to: 'site#index'
 
-  namespace :api do
-    resources :tasks, only: %i[index show create destroy update]
-  end
+  # namespace :api do
+  #   resources :tasks, only: %i[index show create destroy update]
+  # end
 end
